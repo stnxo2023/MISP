c8e3057159bb47a439389f74cddaa204a24f97d2
Jeroen Pinoy <jeroen.pinoy@fortitude.ninja>
2025-01-26 15:42:04 +0100

    fix: [EvenReport:GUI] fix some typos

--- app/webroot/js/markdownEditor/event-report.js
+++ app/webroot/js/markdownEditor/event-report.js
@@ -197,7 +197,7 @@ function pasteImg(cm, event) {
             $('<span>').text('Note that these pictures are not synchronized.')
         )
         const $modalBody = $('<div>').append(
-            $('<p>').text('You\'re about to include a picture in your report. Would you like to add it as an attachment to the event (This will create an attachment Attribute) or should it be a save as a local image (this will not be synchronized).'),
+            $('<p>').text('You\'re about to include a picture in your report. Would you like to add it as an attachment to the event (This will create an attachment Attribute) or should it be saved as a local image (this will not be synchronized)?'),
             $picture,
             $('<div>').append($checkboxContainer),
             $attributeFormContainer,
